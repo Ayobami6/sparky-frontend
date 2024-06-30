b3484cf892f2792232849274a57a7eb231c776ce
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-06-30 23:03:41 +0100

    Complete course data add for benefits and prerequisites

--- app/components/Admin/Courses/CourseOptions.tsx
+++ app/components/Admin/Courses/CourseOptions.tsx
@@ -18,7 +18,7 @@ const CourseOptions = (props: Props) => {
             {
                 options.map((option: any, index: number) => (
                     <div key={index} className={`cursor-pointer hover:text-[#37a39a] text-black dark:text-white ${props.active === index ? "font-[700]" : "font-[400]"} px-4 py-2 transition duration-150`}>
-                        <IoMdCheckmark className={`inline-block mr-2 ${props.active === index ? "text-[#37a39a]" : "text-[#413e3f]"}`} />
+                        <IoMdCheckmark className={`inline-block mr-2 ${props.active >= index ? "text-[#37a39a]" : "text-[#413e3f]"}`} />
                         {option}
                     </div>
                 ))
--- app/components/Admin/Courses/CreateCourse.tsx
+++ app/components/Admin/Courses/CreateCourse.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import CourseInfo from './CourseInfo'
 import CourseOptions from './CourseOptions'
+import CourseData from './CourseData'
 
 type Props = {}
 
@@ -43,6 +44,20 @@ const CreateCourse = (props: Props) => {
                         <CourseInfo
                             courseInfo={courseInfo}
                             setCourseInfo={setCourseInfo}
+                            active={active}
+                            setActive={setActive}
+                        />
+                    )
+                }
+                {
+                    active === 1 && (
+                        <CourseData
+                            courseInfo={courseInfo}
+                            setCourseInfo={setCourseInfo}
+                            benefits={benefits}
+                            setBenefits={setBenefits}
+                            prerequisites={prerequisites}
+                            setPrerequisites={setPrerequisites}
                             active={active}
                             setActive={setActive}
                         />
